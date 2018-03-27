22424ba15d1b8b9d1f06b221d0ce17492dfeef28
Unknown <karimabdelazizmansour@gmail.com>
2018-03-27 03:16:36 +0200

    Add Swifter Framework inside project folder

---
+++ TwitterClient/TwitterClient/Modules/Home/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  TwitterClient
+//
+//  Created by Graphic on 3/27/18.
+//  Copyright © 2018 KarimEbrahem. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+}
